2236f1e727034521bc63ace03c0ff2df28281cd2
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 13:16:13 -0400

    wlan: wifi_hal: Adapt to moved libnl private headers
    Change-Id: Iee157234cd4290489884052d1f5947a5b56cd897

--- wlan/wifi_hal/rtw_wifi_rtt.cpp
+++ wlan/wifi_hal/rtw_wifi_rtt.cpp
@@ -29,8 +29,8 @@
 #include <netlink/object-api.h>
 #include <netlink/netlink.h>
 #include <netlink/socket.h>
-#include <netlink-private/object-api.h>
-#include <netlink-private/types.h>
+#include <nl-priv-dynamic-core/object-api.h>
+#include <linux-private/linux/types.h>
 
 #include "nl80211_copy.h"
 
